4d07c9c813d0d578d944bd13a1e156c180ce33c6
NonoIceOff <54319211+NonoIceOff@users.noreply.github.com>
2024-01-09 11:12:49 +0100

    Classe Personnage

---
+++ src/Personnage.java
@@ -0,0 +1,43 @@
+public class Personnage {
+    private String nom;
+    private int pointsDeVie;
+    private int force;
+
+    public Personnage(String nom, int pointsDeVie, int force) {
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.force = force;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
+
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie = pointsDeVie;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public void attaquer() {
+        System.out.println("Attaque !");
+    }
+
+    public void defendre() {
+        System.out.println("Défense !");
+    }
+}
